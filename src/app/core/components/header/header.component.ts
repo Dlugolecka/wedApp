eb299d491fb8c45409f3552e8575e27279731884
Dlugolecka <ewelina.dlugolecka1210@gmail.com>
2023-10-07 17:04:50 +0200

    Add navigation to home page and login

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  goTOLogin() {
+    {
+      this.router.navigate(['/login'], {});
+    }
+  }
+
+  goToHome() {
+    {
+      this.router.navigate(['/home'], {});
+    }
+  }
+}
