74f7caa8e89f1f2142de2184282c03b237f4d3fe
Dlugolecka <ewelina.dlugolecka1210@gmail.com>
2023-10-07 22:21:52 +0200

    Create login page

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class LoginModule {}
